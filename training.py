8c6074809f4a0b7d0511eeda763135a919dac546
Algrus <38875160+Algrus@users.noreply.github.com>
2020-05-22 20:57:30 -0700

    NN

--- training.py
+++ training.py
@@ -16,17 +16,26 @@ def plotter(xx, sample, T):
 	uhat = T[0,:].to('cpu').detach().numpy()
 	ff = sample['f'][0,0,:].to('cpu').detach().numpy()
 	uu = sample['u'][0,0,:].to('cpu').detach().numpy()
+	mae_error = mae(uhat, uu)
+	l2_error = relative_l2(uhat, uu)
 	plt.figure(figsize=(10,6))
+	plt.title(f'Example\nMAE Error: {mae_error}\nRel. $L_2$ Error: {l2_error}')
+	plt.plot(xx, uu, 'r-', label='$u$')
+	plt.plot(xx, uhat, 'bo', label='$\\hat{u}$')
+	plt.plot(xx, ff, 'g', label='$f$')
 	plt.xlim(-1,1)
 	plt.grid(alpha=0.618)
 	plt.xlabel('$x$')
 	plt.ylabel('$y$')
-	plt.title(f'Example')
-	plt.plot(xx, uu, 'r-', label='$\\alpha$')
-	plt.plot(xx, uhat, 'b--', label='$\\hat{\\alpha}$')
-	plt.plot(xx, ff, 'g', label='$f$')
 	plt.legend(shadow=True)
 	plt.show()
+
+def relative_l2(measured, theoretical):
+	return np.linalg.norm(measured-theoretical, ord=2)/np.linalg.norm(theoretical, ord=2)
+
+def mae(measured, theoretical):
+	return np.linalg.norm(measured-theoretical, ord=1)/len(theoretical)
+
 # Check if CUDA is available and then use it.
 if torch.cuda.is_available():  
   dev = "cuda:0" 
@@ -111,66 +120,62 @@ for epoch in tqdm(range(EPOCHS)):
 # model.load_state_dict(torch.load('model.pt'))
 # model.eval()
 
-def relative_l2(measured, theoretical):
-	return np.linalg.norm(measured-theoretical, ord=2)/np.linalg.norm(theoretical, ord=2)
-
-def mae(measured, theoretical):
-	return np.linalg.norm(measured-theoretical, ord=1)/len(theoretical)
 
 
-L = 2
-for _ in range(L):
-	ff = f[_,0,:].to('cpu').detach().numpy()
-	xx = sample_batch['x'][_,0,:].numpy()
-	uhat = u_pred[_,:].to('cpu').detach().numpy()
-	uu = u[_,:].to('cpu').detach().numpy()
-	mae_error = mae(uhat, uu)
-	l2_error = relative_l2(uhat, uu)
-	plt.figure(L + _ + 1, figsize=(10,6))
-	plt.xlim(-1,1)
-	plt.grid(alpha=0.618)
-	plt.xlabel('$x$')
-	plt.ylabel('$u$')
-	plt.title(f'Example {_+1}\nMAE Error: {mae_error}\nRel. $L_2$ Error: {l2_error}')
-	plt.plot(xx, uu, 'r-', label='$u$')
-	plt.plot(xx, uhat, 'b--', label='$\\hat{u}$')
-	plt.plot(xx, ff, 'g', label='$f$')
-	plt.legend(shadow=True)
-	plt.show()
 
-#Get out of sample data
-FILE = '1000'
-norm_f = normalize(pickle_file=FILE, dim='f')
-norm_f = (norm_f[0].mean().item(), norm_f[1].mean().item())
-print(f"f Mean: {norm_f[0]}\nSDev: {norm_f[1]}")
-transform_f = transforms.Compose([transforms.Normalize([norm_f[0]], [norm_f[1]])])
-test_data = LGDataset(pickle_file=FILE, transform_f=transform_f)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=N, shuffle=True)
-for batch_idx, sample_batch in enumerate(testloader):
-		f = Variable(sample_batch['f']).to(device)
-		u = Variable(sample_batch['u']).to(device)
-		break 
+# L = 2
+# for _ in range(L):
+# 	ff = f[_,0,:].to('cpu').detach().numpy()
+# 	xx = sample_batch['x'][_,0,:].numpy()
+# 	uhat = u_pred[_,:].to('cpu').detach().numpy()
+# 	uu = u[_,:].to('cpu').detach().numpy()
+# 	mae_error = mae(uhat, uu)
+# 	l2_error = relative_l2(uhat, uu)
+# 	plt.figure(L + _ + 1, figsize=(10,6))
+# 	plt.xlim(-1,1)
+# 	plt.grid(alpha=0.618)
+# 	plt.xlabel('$x$')
+# 	plt.ylabel('$u$')
+# 	plt.title(f'Example {_+1}\nMAE Error: {mae_error}\nRel. $L_2$ Error: {l2_error}')
+# 	plt.plot(xx, uu, 'r-', label='$u$')
+# 	plt.plot(xx, uhat, 'b--', label='$\\hat{u}$')
+# 	plt.plot(xx, ff, 'g', label='$f$')
+# 	plt.legend(shadow=True)
+# 	plt.show()
+
+# #Get out of sample data
+# FILE = '1000'
+# norm_f = normalize(pickle_file=FILE, dim='f')
+# norm_f = (norm_f[0].mean().item(), norm_f[1].mean().item())
+# print(f"f Mean: {norm_f[0]}\nSDev: {norm_f[1]}")
+# transform_f = transforms.Compose([transforms.Normalize([norm_f[0]], [norm_f[1]])])
+# test_data = LGDataset(pickle_file=FILE, transform_f=transform_f)
+# testloader = torch.utils.data.DataLoader(test_data, batch_size=N, shuffle=True)
+# for batch_idx, sample_batch in enumerate(testloader):
+# 		f = Variable(sample_batch['f']).to(device)
+# 		u = Variable(sample_batch['u']).to(device)
+# 		break 
 
 
-model2.eval()
-optimizer2.zero_grad()
-u_pred = model2(f)
-L = 2
-for _ in range(L):
-	ff = f[_,0,:].to('cpu').detach().numpy()
-	xx = sample_batch['x'][_,0,:].numpy()
-	uhat = u_pred[_,:].to('cpu').detach().numpy()
-	uu = u[_,:].to('cpu').detach().numpy()
-	mae_error = mae(uhat, uu)
-	l2_error = relative_l2(uhat, uu)
-	plt.figure(L + _ + 1, figsize=(10,6))
-	plt.xlim(-1,1)
-	plt.grid(alpha=0.618)
-	plt.xlabel('$x$')
-	plt.ylabel('$u$')
-	plt.title(f'Example {_+1}\nMAE Error: {mae_error}\nRel. $L_2$ Error: {l2_error}')
-	plt.plot(xx, uu[0], 'r-', label='$u$')
-	plt.plot(xx, uhat, 'b--', label='$\\hat{u}$')
-	plt.plot(xx, ff, 'g', label='$f$')
-	plt.legend(shadow=True)
-	plt.show()
+# model2.eval()
+# optimizer2.zero_grad()
+# u_pred = model2(f)
+# L = 2
+# for _ in range(L):
+# 	ff = f[_,0,:].to('cpu').detach().numpy()
+# 	xx = sample_batch['x'][_,0,:].numpy()
+# 	uhat = u_pred[_,:].to('cpu').detach().numpy()
+# 	uu = u[_,:].to('cpu').detach().numpy()
+# 	mae_error = mae(uhat, uu)
+# 	l2_error = relative_l2(uhat, uu)
+# 	plt.figure(L + _ + 1, figsize=(10,6))
+# 	plt.xlim(-1,1)
+# 	plt.grid(alpha=0.618)
+# 	plt.xlabel('$x$')
+# 	plt.ylabel('$u$')
+# 	plt.title(f'Example {_+1}\nMAE Error: {mae_error}\nRel. $L_2$ Error: {l2_error}')
+# 	plt.plot(xx, uu[0], 'r-', label='$u$')
+# 	plt.plot(xx, uhat, 'b--', label='$\\hat{u}$')
+# 	plt.plot(xx, ff, 'g', label='$f$')
+# 	plt.legend(shadow=True)
+# 	plt.show()
