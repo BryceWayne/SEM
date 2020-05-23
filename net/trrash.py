431d9bc845a08cd8e3f35db7da62948104012d51
Algrus <38875160+Algrus@users.noreply.github.com>
2020-05-23 13:19:27 -0700

    NN

--- net/trrash.py
+++ net/trrash.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 # target output size of 5
-m = nn.AdaptiveMaxPool1d(64)
-input = torch.randn(1, 64, 8)
+m = nn.MaxPool1d(kernel_size=5, stride=1, padding=2)
+input = torch.randn(1, 32, 64)
 output = m(input)
 print(output.shape)
