51c7f2615e5bfcb97a488718059e52057028a2fb
Algrus <38875160+Algrus@users.noreply.github.com>
2020-05-22 20:10:41 -0700

    NN

--- training.py
+++ training.py
@@ -143,7 +143,8 @@ FILE = '1000'
 norm_f = normalize(pickle_file=FILE, dim='f')
 norm_f = (norm_f[0].mean().item(), norm_f[1].mean().item())
 print(f"f Mean: {norm_f[0]}\nSDev: {norm_f[1]}")
-transform_f = transforms.Compose([transforms.Normalize([norm_f[0]], [norm_f[1]])])test_data = LGDataset(pickle_file=FILE, transform_f=transform)
+transform_f = transforms.Compose([transforms.Normalize([norm_f[0]], [norm_f[1]])])
+test_data = LGDataset(pickle_file=FILE, transform_f=transform)
 testloader = torch.utils.data.DataLoader(test_data, batch_size=N, shuffle=True)
 for batch_idx, sample_batch in enumerate(testloader):
 		f = Variable(sample_batch['f']).to(device)
