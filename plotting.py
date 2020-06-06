181a3841dd4c66161c1df9219e703bc7b817d45f
Algrus <38875160+Algrus@users.noreply.github.com>
2020-06-05 20:40:09 -0700

    NN

--- plotting.py
+++ plotting.py
@@ -68,7 +68,7 @@ def plotter(xx, sample, epoch, a=None, u=None, DE=None, title='alpha', ks=7):
 			      f'DE Rel. $L_2$ Error: {np.round(float(l2_error_de), 6)}\n'\
 			      f'DE Rel. $L_\\infty$ Error: {np.round(float(linf_error_de), 6)}')
 		plt.plot(xxx, ff, 'g-', label='$f$')
-		plt.plot(xx, de, 'co', mfc='none', label='ODE')
+		plt.plot(xxx, de, 'co', mfc='none', label='ODE')
 		plt.xlim(-1,1)
 		plt.grid(alpha=0.618)
 		plt.xlabel('$x$')
