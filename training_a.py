1dc5efa04a7dd92721d7514d3ed1003aca8f0f5a
Algrus <38875160+Algrus@users.noreply.github.com>
2020-06-13 18:31:34 -0700

    dtypes

--- training_a.py
+++ training_a.py
@@ -174,4 +174,7 @@ if args.data == True:
 	df.at[df.index[-1],'EPOCHS'] = EPOCHS
 	df.at[df.index[-1],'BATCH'] = BATCH
 	df = df[COLS]
+	_ = ['BATCH', 'EPOCHS', 'AVG IT/S', 'LOSS']
+	for obj in _:
+		df[obj] = df[obj].astype(float)
 	df.to_excel('temp.xlsx')
