58799820d0d507b7611f80480ddb56bf0470ff9a
Algrus <38875160+Algrus@users.noreply.github.com>
2020-06-19 12:15:19 -0700

    logging

---
+++ data_logging.py
@@ -0,0 +1,22 @@
+#logging.py
+import pandas as pd
+import subprocess
+
+COLS = ['TIMESTAMP', 'DATASET', 'FOLDER', 'SHAPE', 'BLOCKS', 'K.SIZE', 'FILTERS', 'BATCH', 'EPOCHS', 'AVG IT/S', 'LOSS', 'MAEa', 'MSEa', 'MIEa', 'MAEu', 'MSEu', 'MIEu']
+def log_data(KERNEL_SIZE, FILE, PATH, BLOCKS, FILTERS, DATA, EPOCHS, N, LOSS, AVG_ITER):
+	if DATA == True:
+		subprocess.call(f'python evaluate_a.py --ks {KERNEL_SIZE} --input {FILE} --path {PATH} --blocks {BLOCKS} --filters {FILTERS} --data True', shell=True)
+		df = pd.read_excel('temp.xlsx')
+		df.at[df.index[-1],'AVG IT/S'] = {AVG_ITER}
+		df.at[df.index[-1],'LOSS'] = {LOSS}
+		df.at[df.index[-1],'EPOCHS'] = {EPOCHS}
+		df.at[df.index[-1],'BATCH'] = {N}
+		df.at[df.index[-1], 'BLOCKS'] = {BLOCKS}
+		df.at[df.index[-1], 'FILTERS'] = {FILTERS}
+		df = df[COLS]
+		_ = ['SHAPE', 'BLOCKS', 'K.SIZE', 'BATCH', 'EPOCHS', 'AVG IT/S', 'LOSS', 'MAEa', 'MSEa', 'MIEa', 'MAEu', 'MSEu', 'MIEu']
+		for obj in _:
+			df[obj] = df[obj].astype(float)
+		df.to_excel('temp.xlsx')
+	else:
+		subprocess.call(f'python evaluate_a.py --ks {KERNEL_SIZE} --input {FILE} --path {PATH} --blocks {BLOCKS}  --filters {FILTERS}', shell=True)
